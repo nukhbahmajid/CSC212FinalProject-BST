e6396198a54f2f0079747df1e0788375b60660b6
Nukhbah Majid <nukhbamajid@gmail.com>
2019-04-19 21:27:43 -0400

    Initialized framework

---
+++ src/main/java/edu/smith/cs/csc212/fp/Main.java
@@ -0,0 +1,11 @@
+package edu.smith.cs.csc212.fp;
+
+public class Main {
+  public static boolean returnsTrue() {
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/edu/smith/cs/csc212/fp/MainTest.java
@@ -0,0 +1,11 @@
+package edu.smith.cs.csc212.fp;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MainTest {
+  @Test
+  public void testMain() {
+    Assert.assertEquals(true, Main.returnsTrue());
+  }
+}
