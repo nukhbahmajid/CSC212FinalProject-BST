33e8e771365da74a58f586c171ebfd84c1c3dfcf
gin2000 <44727063+gin2000@users.noreply.github.com>
2019-04-23 21:38:09 -0400

    create fields

---
+++ src/main/java/edu/smith/cs/csc212/fp/BTrees.java
@@ -0,0 +1,40 @@
+package edu.smith.cs.csc212.fp;
+
+import java.util.List;
+
+public class BTrees<Key extends Comparable<Key>> {
+	private int height;
+	
+	//number of keys stored in nodes
+	public int size;
+	
+	private static class Node {
+		public Node root;
+		//number of nodes
+		Node[] children = new Node[4];
+		//number of keys in one node
+		public int numKeys;
+		//key in the node
+		public String key;
+		//a list of keys in one node
+		public List<String> keys;
+		
+		public Node(int i) {
+			
+		}	
+	}
+	
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void add(Key key) {
+
+	}
+	
+	
+}
